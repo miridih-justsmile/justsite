b8c492c75fe0885127448835bf85a449f227bfe4
wjdrnsl <wjdrnsl@gmail.com>
2020-03-29 17:09:39 +0900

    version-0.1 자동컴파일 시도..

--- src/main/java/comm/justsmile/justsite/springboot/Application.java
+++ src/main/java/comm/justsmile/justsite/springboot/Application.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        // Auto Compile 여부.
-        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(Application.class, args);
     }
 }
