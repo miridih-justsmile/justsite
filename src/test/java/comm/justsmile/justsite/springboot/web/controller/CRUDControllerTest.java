1934c9942eac66a74cc6ca34ff4d770a9f06ed55
wjdrnsl <wjdrnsl@gmail.com>
2020-04-30 22:34:14 +0900

    version-0.5 간단한 UI변경

--- src/test/java/comm/justsmile/justsite/springboot/web/controller/CRUDControllerTest.java
+++ src/test/java/comm/justsmile/justsite/springboot/web/controller/CRUDControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,7 +19,6 @@ public class CRUDControllerTest {
     @Test
     public void 메인페이지_로딩() {
         String body = this.restTemplate.getForObject("/crud", String.class);
-        System.out.println(body);
-        assertTrue(body.contains("스프링부트로 시작하는 웹 서비스"));
+        assertFalse(body, body.contains("error"));
     }
 }
