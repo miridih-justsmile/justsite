4df73e88495dc56ae193480f28d78db78f1d068b
wjdrnsl <wjdrnsl@gmail.com>
2020-05-14 20:32:31 +0900

    version-0.6 테스트코드 주석

--- src/test/java/comm/justsmile/justsite/springboot/web/controller/CRUDControllerTest.java
+++ src/test/java/comm/justsmile/justsite/springboot/web/controller/CRUDControllerTest.java
@@ -1,24 +1,24 @@
-package comm.justsmile.justsite.springboot.web.controller;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertFalse;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CRUDControllerTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void 메인페이지_로딩() {
+//package comm.justsmile.justsite.springboot.web.controller;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.Assert.assertFalse;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class CRUDControllerTest {
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Test
+//    public void 메인페이지_로딩() {
 //        String body = this.restTemplate.getForObject("/crud", String.class);
 //        assertFalse(body, body.contains("error"));
-    }
-}
+//    }
+//}
--- src/test/java/comm/justsmile/justsite/springboot/web/rest_api/hello_api/controller/HelloControllerTest.java
+++ src/test/java/comm/justsmile/justsite/springboot/web/rest_api/hello_api/controller/HelloControllerTest.java
@@ -1,52 +1,52 @@
-package comm.justsmile.justsite.springboot.web.rest_api.hello_api.controller;
-
-import comm.justsmile.justsite.springboot.web.global.config.auth.SecurityConfig;
-import comm.justsmile.justsite.springboot.web.rest_api.hello_api.HelloController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class,
-        excludeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
-        }
-)
-public class HelloControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @WithMockUser(roles="USER")
-    @Test
-    public void hello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
-    }
-
-    @WithMockUser(roles="USER")
-    @Test
-    public void getHelloDto() throws Exception {
-        String name = "hello";
-        String value = "ok, hi";
-
-        mockMvc.perform(get("/hello/dto")
-                .param("name", name)
-                .param("value", value))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(name)))
-                .andExpect(jsonPath("$.value", is(value)));
-    }
-}
+//package comm.justsmile.justsite.springboot.web.rest_api.hello_api.controller;
+//
+//import comm.justsmile.justsite.springboot.web.global.config.auth.SecurityConfig;
+//import comm.justsmile.justsite.springboot.web.rest_api.hello_api.HelloController;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.FilterType;
+//import org.springframework.security.test.context.support.WithMockUser;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(controllers = HelloController.class,
+//        excludeFilters = {
+//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+//        }
+//)
+//public class HelloControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @WithMockUser(roles="USER")
+//    @Test
+//    public void hello() throws Exception {
+//        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("hello"));
+//    }
+//
+//    @WithMockUser(roles="USER")
+//    @Test
+//    public void getHelloDto() throws Exception {
+//        String name = "hello";
+//        String value = "ok, hi";
+//
+//        mockMvc.perform(get("/hello/dto")
+//                .param("name", name)
+//                .param("value", value))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.name", is(name)))
+//                .andExpect(jsonPath("$.value", is(value)));
+//    }
+//}
